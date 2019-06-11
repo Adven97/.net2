51c932cbc8e11e3be9d1cfedfc568e3a8589bfaf
Adven97 <atomczak30@gmail.com>
2019-06-11 23:49:57 +0200

    eventy

---
+++ dynamicArray/DynamicArray.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicznaTablica
+{
+    public delegate void MyDelegate(int tabSize);
+
+    class DynamicArray
+    {
+        private int[] array;
+        private int index = 0;
+        
+        private MyDelegate myDelegate = null;
+
+        public DynamicArray()
+        {
+            array = new int[1];
+            myDelegate += tabSize;
+        }
+        public DynamicArray(int size)
+        {
+            array = new int[size];
+            myDelegate += tabSize;
+        }
+
+        public void tabSize(int sizeOfArray)
+        {
+            Console.WriteLine("Rozmiar tablicy: " + sizeOfArray);
+        }
+
+        public void printAllElements()
+        {
+            for (int i = 0; i < index; i++)
+            {
+                getElement(i);
+            }
+
+            Console.WriteLine("TAB LENGTH: " + array.Length);
+        }
+
+        public void addElement(int x)
+        {
+            if (index >= array.Length)
+            {
+                makeBigger(2 * array.Length);
+            }
+            array[index++] = x;
+
+            myDelegate(array.Length);
+        }
+
+        public void setElement(int indx, int value)
+        {
+            if (indx >= array.Length)
+            {
+                makeBigger(indx + 1);
+                Console.WriteLine("NEW TAB LENGTH: " + array.Length);
+                index = indx;
+            }
+            array[indx] = value;
+
+            Console.WriteLine("New value from index " + indx + " is: " + array[indx]);
+
+        }
+
+
+        public void makeBigger(int ile)
+        {
+            int[] temp = new int[array.Length];
+            Array.Copy(array, temp, index);
+            array = new int[ile];
+            Array.Copy(temp, array, index);
+
+            //dac delegata na event
+        }
+
+        public int getElement(int indx)
+        {
+            if (indx <= index)
+            {
+                Console.WriteLine("Element form index " + indx + " is: " + array[indx]);
+                return array[indx];
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Index poza skala");
+            }
+
+        }
+    }
+}
---
+++ dynamicArray/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicznaTablica
+{
+    class Program
+    {
+        private static int[] array;
+        private static int index = 0;
+        static void Main(string[] args)
+        {
+            DynamicArray array = new DynamicArray(5);
+
+            array.addElement(1);
+            array.addElement(2);
+            array.addElement(21);
+            array.addElement(221);
+            array.addElement(21232);
+            array.addElement(55);
+            array.addElement(567);
+
+
+            array.printAllElements();
+            Console.WriteLine();
+            array.setElement(25, 92);
+            Console.WriteLine();
+
+            try
+            {
+                array.getElement(66);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Exception occured: " + e.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
